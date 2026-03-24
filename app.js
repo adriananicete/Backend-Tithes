bc4ebbd2c10ab8f1bf876f61cc6320fb30917a93
Adrian Anicete <anicete.ian@gmail.com>
2026-03-24 15:13:29 +0800

    feat: change password - all authenticated users can change their password

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import { connectDB } from './src/config/db.js';
 import authRoutes from './src/routes/auth/authRoutes.js';
-import userRoutes from './src/routes/admin/userRoutes.js';
+import adminUserRoutes  from './src/routes/admin/userRoutes.js';
+import userRoutes from './src/routes/userRoutes.js';
 import categoryRoutes from './src/routes/admin/categoryRoutes.js';
 
 const PORT = process.env.PORT || 7002;
@@ -17,8 +18,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/auth', authRoutes);
-app.use('/api/admin/users', userRoutes);
+app.use('/api/admin/users', adminUserRoutes );
 app.use('/api/admin/categories', categoryRoutes);
+app.use('/api/users', userRoutes)
 
 connectDB().then(() => {
     app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
