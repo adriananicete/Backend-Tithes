836a628ee3eaaa8a866d8bd5f5bc34869c46d13e
Adrian Anicete <anicete.ian@gmail.com>
2026-04-04 13:36:48 +0800

    feat: setting up expense routes, controllers and schema

---
+++ src/models/Expense.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const expenseSchema = new mongoose.Schema({
+    source: {
+        type: String,
+        enum: ['voucher','manual'],
+        required: true,
+    },
+    linkedId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Voucher',
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    recordedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {timestamps: true})
+
+export const Expense = mongoose.model('Expense', expenseSchema);
