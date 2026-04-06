ba0919ac537e435866c39221fc16b44a9f4e1edd
Adrian Anicete <anicete.ian@gmail.com>
2026-04-06 11:12:36 +0800

    feat: notifications - auto-trigger on status changes

--- src/controllers/notificationController.js
+++ src/controllers/notificationController.js
@@ -1,28 +1,68 @@
+import mongoose from "mongoose";
+import { Notification } from "../models/Notification.js";
+
 const getNotifications = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: error.message})
-    }
+  try {
+    const getAllNotif = await Notification.find({ userId: req.user.id });
+    if (getAllNotif.length === 0)
+      return res.status(200).json({ message: "Notification Empty" });
+
+    res.status(200).json({
+      status: "Success",
+      count: getAllNotif.length,
+      data: getAllNotif,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 const markAsRead = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: error.message})
-    }
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return res.status(400).json({ error: "Invalid Id!" });
+
+    const findId = await Notification.findById(id);
+    if (!findId)
+      return res.status(400).json({ error: "Notification not found" });
+
+    if (findId.userId.toString() !== req.user.id)
+      return res.status(403).json({ error: "Forbidden" });
+
+     await Notification.findByIdAndUpdate(
+      id,
+      { $set: { isRead: true } },
+      { new: true, runValidators: true },
+    );
+
+    res.status(200).json({
+      status: "Success",
+      message: "Mark as read",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 const markAllAsRead = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({error: error.message})
-    }
+  try {
+    await Notification.updateMany(
+      { userId: req.user.id, isRead: false },
+      { $set: { isRead: true } },
+    );
+
+    res.status(200).json({
+      status: "Success",
+      message: "Mark all as read",
+    });
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
 };
 
-export { getNotifications, markAsRead, markAllAsRead};
+export { getNotifications, markAsRead, markAllAsRead };
